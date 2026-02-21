7bd7ca0797d03f532ccca272bf44c4fb8493ff24
vincenthopf <vincenthopf@users.noreply.github.com>
2026-02-22 00:37:50 +1100

    Add Microsoft Clarity analytics to website

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -49,6 +49,20 @@ export default function RootLayout({
             strategy="lazyOnload"
           />
         )}
+        {process.env.NEXT_PUBLIC_CLARITY_ID && (
+          <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+          >
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+            `}
+          </Script>
+        )}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
